acf3b39d2168e7fb1e9f4c7e44401b24b2fee2bb
Leonardo buta <teste@teste.com>
2024-01-17 21:39:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Quantos numero voce quer digitar ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"valor {0}");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+
+            }
+            Console.WriteLine("soma = " + soma);
+        }
+    }
+}
+
